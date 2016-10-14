9fd8e1192828b441b9040cb0b76dfffb518796e8
Luna3415 <kim.gijbels@student.pxl.be>
2016-10-14 14:37:05 +0200

    SensorModel toegevoegd

---
+++ backend_sensortag/backend_sensortag/src/main/java/be/pxl/groep7/models/SensorModel.java
@@ -0,0 +1,6 @@
+package be.pxl.groep7.models;
+
+public class SensorModel {
+
+	public int id;
+}
--- backend_sensortag/backend_sensortag/src/main/java/test/Test.java
+++ backend_sensortag/backend_sensortag/src/main/java/test/Test.java
@@ -1,10 +1,17 @@
 package test;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Test {
 
-	public static void main(String[] args) {
+	@RequestMapping(method = RequestMethod.GET)
+	public String bla(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("test");
+		System.out.println("test");
+		return "";
 	}
 
 }
