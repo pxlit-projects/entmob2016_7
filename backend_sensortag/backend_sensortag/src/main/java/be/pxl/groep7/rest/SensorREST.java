3564d0fe20d9dbb5cb0ebed20d66e48e54814b63
EerdekensBert <bert.eerdekens@student.pxl.be>
2016-10-14 14:42:48 +0200

    create_package_rest

---
+++ backend_sensortag/backend_sensortag/src/main/java/be/pxl/groep7/rest/SensorREST.java
@@ -0,0 +1,17 @@
+package be.pxl.groep7.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sensor")
+public class SensorREST {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String handleGet(){
+		System.out.println("get");
+		return "get";
+	}
+
+}
