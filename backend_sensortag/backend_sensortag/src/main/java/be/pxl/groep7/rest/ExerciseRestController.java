b568181bf3dcc669c2a97f92bd254d022cdc4c66
Luna3415 <kim.gijbels@student.pxl.be>
2016-10-19 14:34:59 +0200

    Bezig aan de exerciseRestController

--- backend_sensortag/backend_sensortag/src/main/java/be/pxl/groep7/rest/ExerciseRestController.java
+++ backend_sensortag/backend_sensortag/src/main/java/be/pxl/groep7/rest/ExerciseRestController.java
@@ -1,11 +1,21 @@
 package be.pxl.groep7.rest;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import be.pxl.groep7.models.Exercise;
 
 @RestController
 @RequestMapping("exercise")
 public class ExerciseRestController {
-
 	
+	private ExerciseDao dao;
+
+	@RequestMapping(value="{id}", method = RequestMethod.GET, produces = "application/json")
+	public Exercise getExerciseById(@PathVariable("id") int id){
+		
+	}
 }
