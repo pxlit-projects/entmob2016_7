d9ea0da52099559d0541ba34a05e3877118861df
Luna3415 <kim.gijbels@student.pxl.be>
2016-10-19 15:14:35 +0200

    CategoryRestController zou klaar moeten zijn

--- backend_sensortag/backend_sensortag/src/main/java/be/pxl/groep7/rest/CategoryRestController.java
+++ backend_sensortag/backend_sensortag/src/main/java/be/pxl/groep7/rest/CategoryRestController.java
@@ -1,5 +1,37 @@
 package be.pxl.groep7.rest;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import be.pxl.groep7.models.Category;
+import be.pxl.groep7.models.Exercise;
+
+@RestController
+@RequestMapping("/category")
 public class CategoryRestController {
 
+	private CategoryDao dao;
+
+	@RequestMapping(value="{id}", method = RequestMethod.GET, produces = "application/json")
+	public Category getCategoryById(@PathVariable("id") int id){
+		return dao.getCategoryById(id);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public void addCategory(@RequestBody Category category){
+		dao.addCategory(category);
+	}
+	
+	@RequestMapping(value="{id}", method = RequestMethod.PUT)
+	public void editCategory(@PathVariable("id") int id, @RequestBody Category category){
+		dao.editCategory(id, category);
+	}
+	
+	@RequestMapping(value="{id}", method = RequestMethod.DELETE)
+	public void deleteCategory(@PathVariable("id") int id) {
+		dao.deleteCategory(id);
+	}
 }
