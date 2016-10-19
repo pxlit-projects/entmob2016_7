6f72a302a35b47855b5d3ec6e924428c64c49d60
EerdekensBert <bert.eerdekens@student.pxl.be>
2016-10-19 15:08:59 +0200

    dao_classes

--- backend_sensortag/backend_sensortag/src/main/java/be/pxl/groep7/dao/CompletedSetRepositoryImpl.java
+++ backend_sensortag/backend_sensortag/src/main/java/be/pxl/groep7/dao/CompletedSetRepositoryImpl.java
@@ -12,33 +12,33 @@ import be.pxl.groep7.models.CompletedSet;
 @Repository("completedSerieRepository")
 public class CompletedSetRepositoryImpl implements ICompletedSetRepository {
 
-	
-private EntityManagerFactory emf;
-	
+
+	private EntityManagerFactory emf;
+
 	@PersistenceUnit
 	public void setEntityManagerFactory(EntityManagerFactory emf){
 		this.emf = emf;
 	}
-	
+
 	public CompletedSet getCompletedSerieById(int id){
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
-		
+
 		CompletedSet serie = em.find(CompletedSet.class, id);
-		
+
 		tx.commit();
 		em.close();
 		return serie;
 	}
-	
+
 	public void addCompletedSerie(CompletedSet serie){
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
-		
+
 		em.persist(serie);
-		
+
 		tx.commit();
 		em.close();
 		emf.close();
