407147f4fa6786c5c8cd52ed0fc80e7dd0adb659
Dries Vuylsteke <dvuylsteke@gmail.com>
2016-11-10 15:53:47 +0100

    added fitsense model dll

--- FitSense/FitSense/FitSense/ViewModels/CategoriesViewModel.cs
+++ FitSense/FitSense/FitSense/ViewModels/CategoriesViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using FitSense.Constants;
+using FitSense.Dependencies;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,29 @@ using System.Threading.Tasks;
 
 namespace FitSense.ViewModels
 {
-    public class CategoriesViewModel
+    public class CategoriesViewModel : ViewModelBase
     {
+        private IUserDataService userDataService;
+        private INavigationService navigationService;
+
+        public List<String> Categories;
+
+        public RelayCommand GoToCategoryCommand { get; set; }
+
+        public CategoriesViewModel(INavigationService navigationService, IUserDataService userDataService)
+        {
+            this.userDataService = userDataService;
+            this.navigationService = navigationService;
+
+            InitializeCommands();
+        }
+
+        private void InitializeCommands()
+        {
+            GoToCategoryCommand = new RelayCommand(() =>
+            {
+                //MessengerInstance.Send<LoginViewModel>(this, Messages.LoginSucces);
+            });
+        }
     }
 }
