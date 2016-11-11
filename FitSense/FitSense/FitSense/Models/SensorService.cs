522826dc1fefa00efb71b3f38ed360d6eb0080e9
danielmertens <danielmertensfaema@hotmail.com>
2016-11-11 14:14:02 +0100

    added comments to the navigation service

--- FitSense/FitSense/FitSense/Models/SensorService.cs
+++ FitSense/FitSense/FitSense/Models/SensorService.cs
@@ -57,7 +57,6 @@ namespace FitSense.Models
             }
             
         }
-
-
+        
     }
 }
--- FitSense/FitSense/FitSense/Services/NavigationService.cs
+++ FitSense/FitSense/FitSense/Services/NavigationService.cs
@@ -12,6 +12,12 @@ namespace FitSense.Services
 {
     public class NavigationService : INavigationService
     {
+        /// <summary>
+        /// Method makes sure you can get the right page in a simple way.
+        /// </summary>
+        /// <param name="pageName">The constant page name</param>
+        /// <param name="objectToPass">The object to pass to the new page if any.</param>
+        /// <returns>The requested page</returns>
         private Page GetPage(string pageName, object objectToPass = null)
         {
             switch (pageName)
@@ -25,9 +31,14 @@ namespace FitSense.Services
             return null;
         }
 
-        //The navigation property of the root page. (MainView)
+        /// <summary>
+        /// The navigation property of the root page. (MainView) 
+        /// </summary>
         public INavigation Navigation { get; set; }
 
+        /// <summary>
+        /// The modalStack of the App navigation.
+        /// </summary>
         public IReadOnlyList<Page> ModalStack
         {
             get
@@ -36,6 +47,9 @@ namespace FitSense.Services
             }
         }
 
+        /// <summary>
+        /// The navigationStack of the App navigation.
+        /// </summary>
         public IReadOnlyList<Page> NavigationStack
         {
             get
@@ -44,46 +58,89 @@ namespace FitSense.Services
             }
         }
 
+        /// <summary>
+        /// Insert a page in front of a page in the navigationStack.
+        /// </summary>
+        /// <param name="pageName">The page to be inserted</param>
+        /// <param name="beforeName">The page it will be inserted before</param>
         public void InsertPageBefore(string pageName, string beforeName)
         {
             Navigation.InsertPageBefore(GetPage(pageName), GetPage(beforeName));
         }
 
+        /// <summary>
+        /// Go back in the navigation stack
+        /// </summary>
+        /// <returns></returns>
         public Task<Page> PopAsync()
         {
             return Navigation.PopAsync();
         }
 
+        /// <summary>
+        /// Go back in the navigation with animation.
+        /// </summary>
+        /// <param name="animated">True if animation is required</param>
+        /// <returns></returns>
         public Task<Page> PopAsync(bool animated)
         {
             return Navigation.PopAsync(animated);
         }
 
+        /// <summary>
+        /// Go back in the navigation stack
+        /// </summary>
+        /// <returns></returns>
         public Task<Page> PopModalAsync()
         {
             return Navigation.PopModalAsync();
         }
 
+        /// <summary>
+        /// Go back in the navigation with animation.
+        /// </summary>
+        /// <param name="animated">True if animation is required</param>
+        /// <returns></returns>
         public Task<Page> PopModalAsync(bool animated)
         {
             return Navigation.PopModalAsync(animated);
         }
 
+        /// <summary>
+        /// Go back to the root page of the application.
+        /// </summary>
+        /// <returns></returns>
         public Task PopToRootAsync()
         {
             return Navigation.PopToRootAsync();
         }
 
+        /// <summary>
+        /// Go back to the root page of the application with animation.
+        /// </summary>
+        /// <param name="animated">True if animation is required</param>
+        /// <returns></returns>
         public Task PopToRootAsync(bool animated)
         {
             return Navigation.PopToRootAsync(animated);
         }
 
+        /// <summary>
+        /// Insert a page in the navigation stack.
+        /// </summary>
+        /// <param name="pageName">The name of the page. (Found in PageUrls)</param>
+        /// <returns></returns>
         public Task PushAsync(string pageName)
         {
             return Navigation.PushAsync(GetPage(pageName));
         }
 
+        /// <summary>
+        /// Insert a page with a parameter in the navigation stack.
+        /// </summary>
+        /// <param name="pageName">The name of the page. (Found in PageUrls)</param>
+        /// <param name="objectToPass">The object to pass to the new page.</param>
+        /// <returns></returns>
         public Task PushAsync(string pageName, object objectToPass)
         {
             Page page = GetPage(pageName);
@@ -91,11 +148,34 @@ namespace FitSense.Services
             return Navigation.PushAsync(page);
         }
 
+        /// <summary>
+        /// Insert a page in the navigation stack with animation.
+        /// </summary>
+        /// <param name="pageName">The name of the page. (Found in PageUrls)</param>
+        /// <param name="animated">True if animation is required</param>
+        /// <returns></returns>
         public Task PushAsync(string pageName, bool animated)
         {
             return Navigation.PushAsync(GetPage(pageName), animated);
         }
 
+        /// <summary>
+        /// Insert a page in the Modal stack.
+        /// </summary>
+        /// <param name="pageName">The name of the page. (Found in PageUrls)</param>
+        /// <returns></returns>
+        public Task PushModalAsync(string pageName)
+        {
+            Page p = GetPage(pageName);
+            return Navigation.PushModalAsync(p);
+        }
+
+        /// <summary>
+        /// Insert a page with a parameter in the Modal stack.
+        /// </summary>
+        /// <param name="pageName">The name of the page. (Found in PageUrls)</param>
+        /// <param name="objectToPass">The object to pass to the new page.</param>
+        /// <returns></returns>
         public Task PushModalAsync(string pageName, object objectToPass)
         {
             Page page = GetPage(pageName, objectToPass);
@@ -103,20 +183,26 @@ namespace FitSense.Services
             return Navigation.PushModalAsync(page);
         }
 
+        /// <summary>
+        /// Insert a page in the Modal stack with animation.
+        /// </summary>
+        /// <param name="pageName">The name of the page. (Found in PageUrls)</param>
+        /// <param name="animated">True if animation is required</param>
+        /// <returns></returns>
         public Task PushModalAsync(string pageName, bool animated)
         {
             return Navigation.PushModalAsync(GetPage(pageName), animated);
         }
 
+        /// <summary>
+        /// Removes a page from the navigation stack.
+        /// </summary>
+        /// <param name="pageName"></param>
         public void RemovePage(string pageName)
         {
             Navigation.RemovePage(GetPage(pageName));
         }
 
-        public Task PushModalAsync(string pageName)
-        {
-            Page p = GetPage(pageName);
-            return Navigation.PushModalAsync(p);
-        }
+        
     }
 }
