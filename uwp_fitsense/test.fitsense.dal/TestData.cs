e9e7548b22c91728a3de695c7c0938721fdb429d
Dries Vuylsteke <dvuylsteke@gmail.com>
2016-11-20 13:29:37 +0100

    dal test library

--- uwp_fitsense/test.fitsense.dal/TestData.cs
+++ uwp_fitsense/test.fitsense.dal/TestData.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace test_fitsense
+namespace test.fitsense.dal
 {
-    class TestData
+    public class TestData
     {
         public static List<Category> categories = new List<Category>()
         {
--- uwp_fitsense/test.fitsense.dal/mocks/MockCategoryRepository.cs
+++ uwp_fitsense/test.fitsense.dal/mocks/MockCategoryRepository.cs
@@ -2,11 +2,8 @@
 using fitsense.models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace test_fitsense.mocks
+namespace test.fitsense.dal.mocks
 {
     public class MockCategoryRepository : ICategoryRepository
     {
