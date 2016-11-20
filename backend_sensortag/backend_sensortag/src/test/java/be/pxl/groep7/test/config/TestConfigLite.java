283ba4f8d6e436cce66e7c6f7948c56f392b7cc5
Luna3415 <kim.gijbels@student.pxl.be>
2016-11-20 12:17:11 +0100

    De .NET kant get all en post gefixt van categories

--- backend_sensortag/backend_sensortag/src/test/java/be/pxl/groep7/test/config/TestConfigLite.java
+++ backend_sensortag/backend_sensortag/src/test/java/be/pxl/groep7/test/config/TestConfigLite.java
@@ -1,10 +1,8 @@
 package be.pxl.groep7.test.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 
 import be.pxl.groep7.ComponentScanConfig;
 
--- uwp_fitsense/fitsense.DAL/CategoryRepository.cs
+++ uwp_fitsense/fitsense.DAL/CategoryRepository.cs
@@ -6,6 +6,7 @@ using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace fitsense.DAL
 {
@@ -16,16 +17,18 @@ namespace fitsense.DAL
             using (var client = new HttpClient())
             {
                 // troubles for the kim kim
+                //troubles were solved by the kim kim XD
                 client.BaseAddress = new Uri("http://localhost:8081/sensortagapi/category");
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
-                    string.Format("{0}:{1}", "user", "123456"));
+                Convert.ToBase64String(Encoding.UTF8.GetBytes(string.Format("{0}:{1}", "user", "123456"))));
 
-                var myContent = JsonConvert.SerializeObject(categorie);
+
+            var myContent = JsonConvert.SerializeObject(categorie);
                 var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
                 var byteContent = new ByteArrayContent(buffer);
                 byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
                 var test = JsonConvert.SerializeObject(categorie);
-                StringContent postBody = new StringContent(test);
+                StringContent postBody = new StringContent(test, Encoding.UTF8, "application/json");
                 var result = client.PostAsync("", postBody).Result;
                 string resultContent = result.Content.ReadAsStringAsync().Result;
             }
@@ -33,13 +36,13 @@ namespace fitsense.DAL
 
         public List<Category> GetCategories()
         {
-            /*string categories = "http://localhost:8081/sensortagapi/category/all";
+            string categories = "http://localhost:8081/sensortagapi/category/all";
             var uri = new Uri(String.Format("{0}?format=json", categories));
             var client = new HttpClient();
             var response = Task.Run(() =>
             {
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
-                    Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "user", "123456"))));
+                    Convert.ToBase64String(Encoding.UTF8.GetBytes(string.Format("{0}:{1}", "user", "123456"))));
                 return client.GetAsync(uri);
             }).Result;
 
@@ -47,8 +50,8 @@ namespace fitsense.DAL
 
             var result = Task.Run(() => response.Content.ReadAsStringAsync()).Result;
             var root = JsonConvert.DeserializeObject<List<Category>>(result);
-            return root;*/
-            return DummyData.categories;
+            return root;
+            //return DummyData.categories;
         }
     }
 }
