c2f501a2b96db183ab03fd10f5a535786196cdbd
Luna3415 <kim.gijbels@student.pxl.be>
2016-11-21 22:14:46 +0100

    Toevoeging UserRepository + dll

---
+++ uwp_fitsense/fitsense.DAL/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using fitsense.DAL.dependencies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using fitsense.models;
+
+namespace fitsense.DAL
+{
+    public class UserRepository : IUserRepository
+    {
+        public User SearchUser(string userName)
+        {
+            return new User { Name = userName };
+        }
+    }
+}
