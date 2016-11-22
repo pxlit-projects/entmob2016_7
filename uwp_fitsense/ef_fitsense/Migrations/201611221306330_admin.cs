ac3ce851f0ecad4b1cc50c016b6ac5d4453e08e8
Dries Vuylsteke <dvuylsteke@gmail.com>
2016-11-22 14:36:20 +0100

    more ef and some test fixes

---
+++ uwp_fitsense/ef_fitsense/Migrations/201611221306330_admin.cs
@@ -0,0 +1,16 @@
+namespace ef_fitsense.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class admin : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
