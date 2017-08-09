4e0fe6f9956a1a15494dec32b2be7d5c9723ace8
TR1GG3RFINGER <bert_eerdekens@outlook.com>
2017-08-09 19:44:01 +0200

    java_changing_dbCredentials

---
+++ backend_sensortag/backend_sensortag/src/main/java/be/pxl/groep7/TestDbCon.java
@@ -0,0 +1,28 @@
+package be.pxl.groep7;
+
+import java.sql.DriverManager;
+
+
+public class TestDbCon {	
+		
+		public static void main(String[] args) {
+            //String Url = "jdbc:mysql://192.168.182.134:3306;DatabaseName=sensortag;user=root;Password=Pxl1234.";
+			String Url = "jdbc:mysql://192.168.182.134:3306/sensortag";
+			String username = "root";
+			String pass = "Pxl1234.";
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                System.out.println("Trying to connect");
+                java.sql.Connection connection = DriverManager.getConnection(Url, username, pass);
+
+                System.out.println("Connection Established Successfull and the DATABASE NAME IS:"
+                        + connection.getMetaData().getDatabaseProductName());
+            } catch (Exception e) {
+System.out.println("Unable to make connection with DB");
+                e.printStackTrace();
+            }
+        }
+    }
+		
+		
+	
